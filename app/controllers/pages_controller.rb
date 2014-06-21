ef0f7f7920f43d3557524337ea4102faff533a9f
Casey White <casey.r.white@gmail.com>
2014-06-21 00:47:12 -0500

    make robots environment aware and add route and pages controller to serve robots file for each env

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+
+  def robots
+    robots = File.read(Rails.root + "config/robots/robots.#{Rails.env}.txt")
+    render :text => robots, :layout => false, :content_type => "text/plain"
+  end
+end
