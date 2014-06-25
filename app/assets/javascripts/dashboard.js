d02e6862da9968f4378ad2db0b78b4ce01377e0c
Casey White <casey.r.white@gmail.com>
2014-06-25 00:27:37 -0500

    add devise module to dashboard app and configure AuthProvider

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -2,16 +2,33 @@
 //= require_self
 //= require_tree ./dashboard-app
 
-var dashboard = angular.module('dashboardApp', ['ngResource', 'ngRoute', 'templates']);
+var dashboard = angular.module('dashboardApp', [
+    'ngResource',
+    'ngRoute',
+    'templates',
+    'Devise'
+]);
 
-dashboard.config(['$httpProvider', '$locationProvider', '$routeProvider', function($httpProvider, $locationProvider, $routeProvider) {
-    // Add CSRF Token to all API request to accommodate Rails' built in CSRF protection
-    var authToken = $("meta[name=\"csrf-token\"]").attr("content");
-    $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
+dashboard.config([
+    '$httpProvider',
+    'AuthProvider',
+    '$routeProvider',
+    function($httpProvider, AuthProvider, $routeProvider) {
 
-    $routeProvider
-        .when('/', {
-            templateUrl: 'dashboard/dashboard-home.html',
-            controller: 'HomeCtrl'
-        });
-}]);
+        // Add CSRF Token to all API requests for compatibility with Rails CSRF protection
+        var authToken = $("meta[name=\"csrf-token\"]").attr("content");
+        $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
+
+        // Configure AngularDevise AuthProvider
+        AuthProvider.loginPath('/dashboard/login.json')
+        AuthProvider.logoutPath('/dashboard/logout.json')
+
+        // Configure routes relative to Rails dashboard_path,
+        // i.e. Angular root is at '/dashboard#/'
+        $routeProvider
+            .when('/', {
+                templateUrl: 'dashboard/dashboard-home.html',
+                controller: 'HomeCtrl'
+            });
+    }
+]);
--- app/assets/javascripts/sixty-third.js
+++ app/assets/javascripts/sixty-third.js
@@ -1,16 +1,25 @@
 //= require_self
 //= require_tree ./sixty-third-app
 
-var app = angular.module('sixtyThirdApp', ['ngResource', 'ngRoute', 'templates']);
+var app = angular.module('sixtyThirdApp', [
+    'ngResource',
+    'ngRoute',
+    'templates'
+]);
 
-app.config(['$httpProvider', '$routeProvider', function($httpProvider, $routeProvider) {
-    // Add CSRF Token to all API request to accommodate Rails' built in CSRF protection
-    var authToken = $("meta[name=\"csrf-token\"]").attr("content");
-    $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
+app.config([
+    '$httpProvider',
+    '$routeProvider',
+    function($httpProvider, $routeProvider) {
 
-    $routeProvider
-        .when('/', {
-            templateUrl: 'sixty-third/home.html',
-            controller: 'HomeCtrl'
-        });
-}]);
+        // Add CSRF Token to all API request to accommodate Rails' built in CSRF protection
+        var authToken = $("meta[name=\"csrf-token\"]").attr("content");
+        $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'sixty-third/home.html',
+                controller: 'HomeCtrl'
+            });
+    }
+]);
