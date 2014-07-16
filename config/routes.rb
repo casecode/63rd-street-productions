1d8c068a192baa5165a28f74193d422587e907a5
Casey White <casey.r.white@gmail.com>
2014-07-15 22:07:19 -0500

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
 
   root 'main#public_home'
   get '/robots.txt' => 'main#robots'
+
+  resources :users
+  devise_for :users, :skip => [:sessions, :passwords]
+  as :user do
+    post '/login' => 'devise/sessions#create', defaults: {format: :json}, :as => :user_session
+    delete '/logout' => 'devise/sessions#destroy', defaults: {format: :json}, :as => :destroy_user_session
+  end
 end
