ac6467d6d136e60719544c39109073f1f04db150
Casey White <casey.r.white@gmail.com>
2014-07-15 22:10:45 -0500

    have devise respond to html and json

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,10 @@ module SixtyThird
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Have Devise respond to html and json
+    config.to_prepare do
+      DeviseController.respond_to :html, :json
+    end
   end
 end
